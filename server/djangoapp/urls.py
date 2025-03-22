1dfb1d3f1d0ec39705800ec6b76fe7e2c1f91758
Dylan <dellis2031@gmail.com>
2025-03-21 20:27:18 -0400

    Some dynamic pages.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 
 
     # get dealers
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # get dealer details
